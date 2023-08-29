0808c00c0cdcbefc85cb32339de5647c93b050e3
dariaorlova <orlova77m@gmail.com>
2023-08-29 14:55:24 +0300

    Переделала работу в соответствии с замечаниями преподавателя

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,27 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("ЗАДАЧА 1");
-        byte A = 100;
-        short B = 30_000;
-        int C = 2_000_000_000;
-        long D = 8L;
-        float E = 0.6f;
-        double F = 0.15;
-        System.out.println("Значение переменной A с типом byte равно " + A);
-        System.out.println("Значение переменной B с типом short равно " + B);
-        System.out.println("Значение переменной C с типом int равно " + C);
-        System.out.println("Значение переменной D с типом long равно " + D);
-        System.out.println("Значение переменной E с типом float равно " + E);
-        System.out.println("Значение переменной F с типом double равно " + F);
+        byte a = 100;
+        short b = 30_000;
+        int c = 2_000_000_000;
+        long d = 8L;
+        float e = 0.6f;
+        double f = 0.15;
+        System.out.println("Значение переменной A с типом byte равно " + a);
+        System.out.println("Значение переменной B с типом short равно " + b);
+        System.out.println("Значение переменной C с типом int равно " + c);
+        System.out.println("Значение переменной D с типом long равно " + d);
+        System.out.println("Значение переменной E с типом float равно " + e);
+        System.out.println("Значение переменной F с типом double равно " + f);
 
         System.out.println("ЗАДАЧА 2");
-        float G = 27.12f;
-        long H = 987_678_965_549L;
-        float I = 2.789f;
-        short J = 569;
-        short K = -159;
-        short L = 27_897;
-        byte M = 67;
+        float g = 27.12f;
+        long h = 987_678_965_549L;
+        float i = 2.789f;
+        short j = 569;
+        short k = -159;
+        short l = 27_897;
+        byte m = 67;
         System.out.println("Для решения потребовалось 4 типа данных");
 
         System.out.println("ЗАДАЧА 3");
@@ -37,8 +37,8 @@ public class Main {
         byte capacity1min = 16 / 2;
         byte time20min = 20;
         short time24hour = 24 * 60;
-        short time3day = 3 * 24 * 60;
-        int time1month = 30 * 24 * 60; // если в месяце 30 дней
+        int time3day = 3 * time24hour;
+        int time1month = 10 * time3day; // если в месяце 30 дней
         int capacity20min = capacity1min * time20min;
         int capacity24hour = capacity1min * time24hour;
         int capacity3day = capacity1min * time3day;
@@ -49,18 +49,28 @@ public class Main {
         System.out.println("За 1 месяц машина произвела " + capacity1month + " штук бутылок");
 
         System.out.println("ЗАДАЧА 5");
-        byte canOfPaint = 120;
-        byte canForRoom = 6;
-        int room = canOfPaint / canForRoom;
-        int whitePaint = room * 2;
-        int brownPaint = room * 4;
+        byte whiteCanForRoom = 2;
+        byte brownCanForRoom = 4;
+        int canForRoom = whiteCanForRoom + brownCanForRoom;
+        byte canOfPaintTotal = 120;
+        int room = canOfPaintTotal / canForRoom;
+        int whitePaint = room * whiteCanForRoom;
+        int brownPaint = room * brownCanForRoom;
         System.out.println("В школе, где " + room + " классов, нужно " + whitePaint + " банок белой краски и " + brownPaint + " банок коричневой краски");
 
         System.out.println("ЗАДАЧА 6");
-        short bananas = 5 * 80;
-        short milk = 2 * 105;
-        short iceCream = 2 * 100;
-        short eggs = 4 * 70;
+        byte weightOfBanana = 80;
+        byte volumeOfBananas = 5;
+        byte weightOfMilk = 105;
+        byte volumeOfMilk = 2;
+        byte weightOfIceCream = 100;
+        byte volumeOfIceCream = 2;
+        byte weightOfEggs = 70;
+        byte volumeOfEggs = 4;
+        int bananas = weightOfBanana * volumeOfBananas;
+        int milk = weightOfMilk * volumeOfMilk;
+        int iceCream = weightOfIceCream * volumeOfIceCream;
+        int eggs = weightOfEggs * volumeOfEggs;
         int weightInGrams = bananas + milk + iceCream + eggs;
         float weightInKilograms = (float) weightInGrams / 1000;
         System.out.println("Фитнес-завтрак будет весить " + weightInGrams + " граммов или " + weightInKilograms + " килограмм");
@@ -79,19 +89,13 @@ public class Main {
         int monthlySalaryOfMariaInitial = 67_760;
         int monthlySalaryOfDenisInitial = 83_690;
         int monthlySalaryOfKrisInitial = 76_230;
-        int annualSalaryOfMariaInitial = monthlySalaryOfMariaInitial * 12;
-        int annualSalaryOfDenisInitial = monthlySalaryOfDenisInitial * 12;
-        int annualSalaryOfKrisInitial = monthlySalaryOfKrisInitial * 12;
         float salaryGrowth = 1.1f;
         float monthlySalaryOfMariaFinal = monthlySalaryOfMariaInitial * salaryGrowth;
         float monthlySalaryOfDenisFinal = monthlySalaryOfDenisInitial * salaryGrowth;
         float monthlySalaryOfKrisFinal = monthlySalaryOfKrisInitial * salaryGrowth;
-        float annualSalaryOfMariaFinal = monthlySalaryOfMariaFinal * 12;
-        float annualSalaryOfDenisFinal = monthlySalaryOfDenisFinal * 12;
-        float annualSalaryOfKrisFinal = monthlySalaryOfKrisFinal * 12;
-        float salaryDifferenceForMaria = annualSalaryOfMariaFinal - annualSalaryOfMariaInitial;
-        float salaryDifferenceForDenis = annualSalaryOfDenisFinal - annualSalaryOfDenisInitial;
-        float salaryDifferenceForKris = annualSalaryOfKrisFinal - annualSalaryOfKrisInitial;
+        float salaryDifferenceForMaria = (monthlySalaryOfMariaFinal - monthlySalaryOfMariaInitial) * 12;
+        float salaryDifferenceForDenis = (monthlySalaryOfDenisFinal - monthlySalaryOfDenisInitial) * 12;
+        float salaryDifferenceForKris = (monthlySalaryOfKrisFinal - monthlySalaryOfKrisInitial) * 12;
         System.out.println("Мария теперь получает " + monthlySalaryOfMariaFinal + " рублей в месяц. Годовой доход вырос на " + salaryDifferenceForMaria + " рублей");
         System.out.println("Денис теперь получает " + monthlySalaryOfDenisFinal + " рублей в месяц. Годовой доход вырос на " + salaryDifferenceForDenis + " рублей");
         System.out.println("Кристина теперь получает " + monthlySalaryOfKrisFinal + " рублей в месяц. Годовой доход вырос на " + salaryDifferenceForKris + " рублей");
